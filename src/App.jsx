567c608f507d167e5ec50a62d4212a4416988eb3
YiTing07 <hevecandy@gmail.com>
2025-11-06 21:58:16 +0800

    feat: add routing with react-router-dom in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Task from './pages/Task';
+import Habit from './pages/Habit';
+
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Task />} />
+          <Route path="/habit" element={<Habit />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   )
 }
 
