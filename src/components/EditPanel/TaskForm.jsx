60e63a8c5418dfb19db3884c568c74b2f9601556
YiTing07 <hevecandy@gmail.com>
2025-12-04 15:57:00 +0800

    feat: use TaskContext in Task.jsx and TaskForm.jsx

--- src/components/EditPanel/TaskForm.jsx
+++ src/components/EditPanel/TaskForm.jsx
@@ -1,17 +1,64 @@
+import { useState, useEffect } from "react";
+import { useTasks } from "../../context/TaskContext";
 
+export default function TaskForm({ handleCloseEdit, editingTask }) {
+  const [title, setTitle] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [priority, setPriority] = useState("low");
+  const { addTask, editTask } = useTasks();
 
-export default function TaskForm() {
+  useEffect(() => {
+    if (editingTask) {
+      setTitle(editingTask.title || "");
+      setDeadline(editingTask.deadline || "");
+      setPriority(editingTask.priority || "low")
+    }
+  }, [editingTask])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // 若 title 空白就 return
+    if (!title.trim()) return;
+
+    if(editingTask) {
+      editTask(editingTask.id, title, deadline, priority)
+      handleCloseEdit();
+    } else {
+      addTask({
+        title,
+        deadline,
+        priority,
+      });
+      setTitle("");
+      setDeadline("");
+      setPriority("low");
+      handleCloseEdit()
+    }
+
+    console.log({ title, deadline, priority });
+  }
+
+  const handleCancel = () => {
+    setTitle("");
+    setDeadline("");
+    setPriority("");
+
+    handleCloseEdit()
+  }
 
   return (
     <div className="h-screen mx-auto px-10 ">
       <div className="h-full w-[80%] m-auto sm:w-[70%]">
         <h2 className="mb-6 text-xl font-bold text-base-content">編輯任務</h2>
-        <form className="h-full flex flex-col">
+        <form onSubmit={handleSubmit} className="h-full flex flex-col">
           <div className="grow-6">
             <div className="task-name mb-6">
               <label htmlFor="" className="mr-3" >任務名稱</label>
               <input 
                 type="text" 
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 className="w-50 p-2 bg-base-200 border border-base-content"                
               />
             </div>
@@ -20,6 +67,8 @@ export default function TaskForm() {
               <label htmlFor="" className="mr-3">截止時間</label>
               <input 
                 type="date" 
+                value={deadline}
+                onChange={(e) => setDeadline(e.target.value)}
                 className="w-50 p-2 bg-base-200 border border-base-content " 
               />
             </div>
@@ -29,6 +78,8 @@ export default function TaskForm() {
               <select 
                 name="priority-group"  
                 id=""
+                value={priority}
+                onChange={(e) => setPriority(e.target.value)} 
                 className="w-50 p-2 bg-base-200 border border-base-content"
                 >
                 <option value="low">低</option>
@@ -41,6 +92,7 @@ export default function TaskForm() {
           <div className="button-group flex justify-between grow-5">
             <button 
               type="button"
+              onClick={handleCancel} 
               className="w-20 sm:w-30 h-10 bg-base-200 text-base-content border rounded-(--radius-field) cursor-pointer hover:bg-base-content hover:text-base-200" 
             >
               取消
--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,67 +1,75 @@
-import { AiOutlineEdit } from "react-icons/ai";
+import { AiOutlineEdit, AiOutlineDelete, AiFillStar } from "react-icons/ai";
 import { useState } from "react";
+import { useTasks } from "../context/TaskContext";
+import EditPanel from "../components/EditPanel/EditPanel";
 
-// 臨時的 task data
-const initialTasks = [
-  {
-  "id": "task-001",
-  "title": "準備期中報告",
-  "completed": false,
-},
-{
-  "id": "task-002",
-  "title": "買牛奶",
-  "completed": false,
-},
-{
-  "id": "task-003",
-  "title": "線上聽課14：00",
-  "completed": false,
-},
-{
-  "id": "task-004",
-  "title": "搜尋旅行相關資料",
-  "completed": false,
-},
-]
+const PriorityStar = ({ priority }) => {
+  const starCount = { low: 1, middle: 2, high: 3 };
+  const stars = Array.from({length: starCount[priority] || 0})
+
+  return (
+    <div className="flex justify-center">
+      {stars.map((__, index) => (
+        <AiFillStar key={index} className="text-amber-300" />
+      ))}
+    </div>
+  )
+}
 
 export default function Task() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const { tasks, toggleTask, deleteTask } = useTasks();
 
-  const toggleTask = (id) => {
-    setTasks(prev => 
-      prev.map(task =>
-        task.id === id ? {...task, completed:!task.completed} : task
-      )
-    )
+  const [editOpen, setEditOpen] = useState(false);
+  const [editingTask, setEditingTask] = useState(null);
+
+  const handleEditClick = (task) => {
+    setEditingTask(task);
+    setEditOpen(true);
+  }
+
+  const handleEditClose = () => {
+    setEditOpen(false);
+    setEditingTask(null);
   }
 
   return (
     <div className="h-dvh">
       <div className="flex flex-wrap gap-5 p-8">
         {tasks.map((item) => (
-          <div 
-            key={item.id} 
-            className={`relative w-40 h-40 p-3 border rounded-xl cursor-pointer transition-colors duration-200
-              ${
-                item.completed 
+          <div
+            key={item.id}
+            className={`relative w-30 h-30 p-3 border rounded-xl cursor-pointer transition-colors duration-200
+              ${item.completed
                 ? "bg-gray-100 text-gray-400 line-through"
                 : "border-amber-400 bg-white text-black hover:bg-amber-50"
               }
             `}
-            
           >
-            <AiOutlineEdit className="absolute top-2 right-3" size={20} />
+            <AiOutlineDelete
+              className="absolute top-2 left-3"
+              size={20}
+              onClick={() => deleteTask(item.id)}
+            />
+            <AiOutlineEdit
+              className="absolute top-2 right-3"
+              size={20}
+              onClick={() => handleEditClick(item)}
+            />
 
-            <div 
-              className="flex justify-center items-center h-full" 
+            <div
+              className="flex flex-col justify-evenly h-full mt-4"
               onClick={() => toggleTask(item.id)}
             >
               <p className="text-center">{item.title}</p>
+              <PriorityStar priority={item.priority} />
+              <p className="text-center text-xs text-warning">{item.deadline}</p>
             </div>
           </div>
         ))}
-      </div>    
+      </div>
+
+      <EditPanel isOpen={editOpen} onCloseEdit={handleEditClose} editingTask={editingTask} />
+
     </div>
   )
 }
